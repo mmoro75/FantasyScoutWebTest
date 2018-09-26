1bec8c05b03d8c854d0f184064a91cf6665aa981
Marco Moro <marco.moro@thomsonreuters.com>
2018-09-26 12:04:20 +0100

    deleted not related tests

--- src/test/java/Web_Testing_Soccer/FantasyFootballScout/FantasyScoutTest.java
+++ src/test/java/Web_Testing_Soccer/FantasyFootballScout/FantasyScoutTest.java
@@ -5,13 +5,17 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.awt.RenderingHints;
+import java.awt.RenderingHints.Key;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ById;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -133,137 +137,8 @@ public class FantasyScoutTest {
 	   
 	    
 	  }
-	  
-	  
-	  @Test
-	  public void StaticDropdown() throws Exception {
-		driver.get("https://www.fantasyfootballscout.co.uk/register/");
-
-		Select s = new Select(driver.findElement(By.id("country_of_residence")));
-		s.selectByValue("ITA");
-		System.out.println(driver.findElement(By.id("country_of_residence")).getText());
-	    
-	  }
-	  
-	  
-	  @Test
-	  public void DynamicDropdown() throws Exception {
-		driver.get("https://spicejet.com/");
-	    
-		driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
-		
-		driver.findElement(By.xpath("//a[@value='BKK']")).click();
-		
-		driver.findElement(By.xpath("(//a[@value='DXB'])[2]")).click();
-		
-		
-		
-	    
-	  }
-	  
-	  
-	  
-	  @Test
-	  public void CheckBox() throws Exception {
-		driver.get("https://www.fantasyfootballscout.co.uk/register/");
-		
-		assertFalse("false", driver.findElement(By.xpath("//p//input[@value='yes']")).isSelected());
-
-		driver.findElement(By.xpath("//p//input[@value='yes']")).click();
-		
-		
-		assertTrue("true", driver.findElement(By.xpath("//p//input[@value='yes']")).isSelected());
-		
-	  
-	  }
-	  
+	   
 	 
-	  @Test
-	  public void RadioButton() throws Exception {
-		driver.get("https://www.fantasyfootballscout.co.uk/register/");
-		
-		
-
-		driver.findElement(By.xpath("//input[@value='yes']")).click();
-		
-		// count Radio button in the page with same attribute 
-		
-		System.out.println(driver.findElements(By.xpath("//input[@name='email_accept']")).size());
-	  
-	  
-	  
-	  // the for loop increments until finds second button and click it (2 buttons index 1)
-	  
-	  
-	  int count =driver.findElements(By.xpath("//input[@name='email_accept']")).size();
-
-	  for (int i=0;i<count;i++) {
-	  	
-	  	driver.findElements(By.xpath("//input[@name='email_accept']")).get(1).click();
-	  	
-	    }
-	  
-	  
-	  /*
-	   in this scenario foor look click every radio button in the page as the variable (i) incemenet through the loop 
-	  
-	   int count =driver.findElements(By.xpath("//input[@name='email_accept']")).size();
-
-	  for (int i=0;i<count;i++) {
-	  	
-	  	driver.findElements(By.xpath("//input[@name='email_accept']")).get(i).click();
-	  	
-	    }
-	  
-	  
-	  */
-	  
-	  // get the attributes for the radio buttons in the page and print them out the loop is 
-
-	  for (int i=0;i<count;i++) {
-	  	
-		  System.out.println(driver.findElements(By.xpath("//input[@name='email_accept']")).get(i).getAttribute("value"));
-	  	
-	  }
-	  
-	  // now we want to click on radio button with value yes 
-	  
-	  for (int i=0;i<count;i++) {
-			
-			String text = driver.findElements(By.xpath("//input[@name='email_accept']")).get(i).getAttribute("value");
-			
-			if (text.equals("yes")) {
-				
-				driver.findElements(By.xpath("//input[@name='email_accept']")).get(i).click();
-				
-			}
-
-		}
-
-	  
-	  }
-	  
-	  @Test
-	  public void SeleniumMethods() throws Exception {
-		driver.get("https://www.fantasyfootballscout.co.uk/");
-	    
-		// check if an element is in hidden mode
-	    System.out.println(driver.findElement(By.xpath("//div[@class='login-here alive']")).isDisplayed()); 
-		
-	    // extract text from the specific location for validation 
-	    System.out.println(driver.findElement(By.xpath("//div[@class='login-here alive']")).getText());
-	    
-	    // check if an element exists in a page and how many  
-	    System.out.println(driver.findElements( By.xpath("//div[@class='login-here alive']")).size()); 
-	    
-	    // check whetjer the element is enabled or disabled 
-	    System.out.println(driver.findElement(By.xpath("//div[@class='login-here alive']")).isEnabled());
-	    
-	  }
-		
-	
-
-	
 	  @After
 	  public void tearDown() throws Exception {
 	    driver.quit();
